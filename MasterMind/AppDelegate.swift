d7c1ba989ad072236e3f23c9ed74efc228deab8d
Tomas Gallucci <professortom@gmail.com>
2019-03-28 18:56:03 -0500

    Initial Commit

---
+++ MasterMind/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MasterMind
+//
+//  Created by Tomas Gallucci on 3/28/19.
+//  Copyright © 2019 Tomas Gallucci. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MasterMind/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MasterMind
+//
+//  Created by Tomas Gallucci on 3/28/19.
+//  Copyright © 2019 Tomas Gallucci. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
