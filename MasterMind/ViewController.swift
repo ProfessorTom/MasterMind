90e62a09fda6c2b1e2e09ce7f9ca2be3418e3735
Tomas Gallucci <professortom@gmail.com>
2019-03-28 21:31:54 -0500

    set up models

--- MasterMind/ViewController.swift
+++ MasterMind/ViewController.swift
@@ -8,7 +8,10 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+    
+    var answer = ""
+    var guesses = [String]()
 
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
@@ -18,6 +21,10 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
 
     override var representedObject: Any? {
         didSet {
@@ -26,6 +33,24 @@ class ViewController: NSViewController {
     }
 
     @IBAction func submitGuess(_ sender: Any) {
+    }
+    
+    func result(for guess: String) -> String {
+        return "Result"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw - tableView.makeView(withIdentifier: tableColumn?.identifier, owner: self) as? NSTableCellView else {return nil}
+        
+        if tableColumn?.title == "Guess" {
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
     }
     
 }
